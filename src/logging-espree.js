ef4a2c3830d2a88ce028c6da707203e0ff3f60f3
usuario <alu0101392561@ull.edu.es>
2023-02-28 20:41:20 +0000

    Transpile previo añadido

--- src/logging-espree.js
+++ src/logging-espree.js
@@ -5,6 +5,14 @@ import * as fs from "fs/promises";
 
 export async function transpile(inputFile, outputFile) {
   // Fill in the code here
+  const ast = p.parse(expression);
+  let preamble = '#!/usr/bin/env node\n';
+  //console.log(JSON.stringify(ast, null, 2))
+  let output = preamble + escodegen.generate(ast);
+  if (outputFile === undefined) {
+    console.log(output);
+    return;
+  }
 }
 
 export function addLogging(code) {
@@ -25,5 +33,4 @@ function addBeforeCode(node) {
   var beforeCode = "console.log('Entering " + name + "()');";
   var beforeNodes = espree.parse(beforeCode).body;
   node.body.body = beforeNodes.concat(node.body.body);
-  
 }
